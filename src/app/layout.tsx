27debcb57a8b0075b491e5abd2bb2c30cc45f360
gigabit-eth <ogpuzzle9@gmail.com>
2025-08-31 08:53:19 -0700

    feat(analytics): add Google Tag Manager script to layout
    Implement GTM tracking by adding script and noscript fallback to enable analytics and tracking functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Bungee, Fugaz_One, Inconsolata, Permanent_Marker } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -54,9 +55,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TTNRZCCR');
+          `,
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${bungee.variable} ${fugazOne.variable} ${inconsolata.variable} ${permanentMarker.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TTNRZCCR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
